8dce4016702fb453ee32fd93ac9dbb52874752a2
Rahul1582 <rahulkpatro@gmail.com>
2023-10-16 19:50:02 +0530

    added preloader on start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Sidenav from "./components/Sidenav/Sidenav";
 import Footer from "./components/Footer/Footer";
 import routes from "./routes.js";
-import { Box, useColorMode } from "@chakra-ui/react";
+import { Box, Flex, Image, useColorMode } from "@chakra-ui/react";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+import SpinnerDark from "./assets/spinner-dark.svg";
+import SpinnerLight from "./assets/spinner-light.svg";
 
 export default function App() {
   const { colorMode } = useColorMode();
+  const [contentNotLoaded, setContentNotLoaded] = useState(true);
+
   const redirectRoute = (routes) => {
     return routes.map((route, key) => {
       return (
@@ -14,6 +19,18 @@ export default function App() {
       );
     });
   };
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setContentNotLoaded(false);
+    };
+
+    window.addEventListener("load", handleLoad);
+
+    return () => {
+      window.removeEventListener("load", handleLoad);
+    };
+  }, []);
 
   const StarryBackground = () => {
     const numberOfStars = 100;
@@ -34,39 +51,72 @@ export default function App() {
     return <div className="starry-background">{stars}</div>;
   };
 
+  const spinnerStyle = {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)"
+  };
+
   return (
     <HashRouter>
-      <StarryBackground />
-      <Sidenav routes={routes} />
-      <Box
-        float="right"
-        height="100%"
-        overflow="auto"
-        position="relative"
-        maxHeight="100%"
-        w={{ sm: "100%", xl: "calc( 100% - 290px )" }}
-        maxWidth={{ sm: "100%", xl: "calc( 100% - 290px )" }}
-        transition="all 0.33s cubic-bezier(0.685, 0.0473, 0.346, 1)"
-        transitionDuration=".2s, .2s, .35s"
-        transitionProperty="top, bottom, width"
-        transitionTimingFunction="linear, linear, ease"
-      >
-        <Box
-          mx="auto"
-          p={{ base: "20px", md: "30px" }}
-          pe="20px"
-          minH={{ base: "85vh", xl: "90vh" }}
-          pt="50px"
-        >
-          <Routes>
-            {redirectRoute(routes)}
-            <Route path="*" element={<Navigate to="/home" replace />} />
-          </Routes>
-        </Box>
-        <Box>
-          <Footer />
-        </Box>
-      </Box>
+      {contentNotLoaded ? (
+        colorMode === "light" ? (
+          <>
+            <Flex
+              justifyContent="center"
+              alignItems="center"
+              style={spinnerStyle}
+            >
+              <Image src={SpinnerLight} width="100px" height="100px"></Image>
+            </Flex>
+          </>
+        ) : (
+          <>
+            <Flex
+              justifyContent="center"
+              alignItems="center"
+              style={spinnerStyle}
+            >
+              <Image src={SpinnerDark} width="100px" height="100px"></Image>
+            </Flex>
+          </>
+        )
+      ) : (
+        <>
+          <Sidenav routes={routes} />
+          <StarryBackground />
+          <Box
+            float="right"
+            height="100%"
+            overflow="auto"
+            position="relative"
+            maxHeight="100%"
+            w={{ sm: "100%", xl: "calc( 100% - 290px )" }}
+            maxWidth={{ sm: "100%", xl: "calc( 100% - 290px )" }}
+            transition="all 0.33s cubic-bezier(0.685, 0.0473, 0.346, 1)"
+            transitionDuration=".2s, .2s, .35s"
+            transitionProperty="top, bottom, width"
+            transitionTimingFunction="linear, linear, ease"
+          >
+            <Box
+              mx="auto"
+              p={{ base: "20px", md: "30px" }}
+              pe="20px"
+              minH={{ base: "85vh", xl: "90vh" }}
+              pt="50px"
+            >
+              <Routes>
+                {redirectRoute(routes)}
+                <Route path="*" element={<Navigate to="/home" replace />} />
+              </Routes>
+            </Box>
+            <Box>
+              <Footer />
+            </Box>
+          </Box>
+        </>
+      )}
     </HashRouter>
   );
 }
